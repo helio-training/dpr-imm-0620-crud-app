aa83e26d5e78e23c6d8ee4c3b2e701fa260fa406
Wes Cutting <wesleycutting@gmail.com>
2020-08-04 15:06:19 -0600

    Implemented Upsert in UI

--- presentation/src/components/Game.js
+++ presentation/src/components/Game.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UpsertGame from './UpsertGame'
 
 // const Game = (props) => {
 const Game = ({game, deleteGame, refresh}) => {
+    const [update, setUpdate] = useState(false);
+    const toggleForm = () => setUpdate(!update);
     return (
         <li>
             {game.title}
-            <button className="del-btn"
-                onClick={() => deleteGame(game._id, refresh)}>
-                    x
-            </button>
+            <i onClick={() => toggleForm()}
+                class="fa fa-gear"></i>
+            
+            <i onClick={() => deleteGame(game._id, refresh)} 
+                className="fa fa-trash-o del-btn"></i>
+            
+            {update ? 
+                <UpsertGame game={game}
+                    toggleForm={toggleForm}
+                    refresh={refresh}/> 
+                : ''}
         </li>
     )
 }
---
+++ presentation/src/components/UpsertGame.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+export default class UpsertGame extends React.Component {
+    state = {
+        title: this.props.game.title,
+        developer: this.props.game.developer,
+        platforms: this.props.game.platforms
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const api_url = process.env.REACT_APP_API_URL;
+        fetch(`${api_url}/games/${this.props.game._id}`, {
+            method: "PUT", 
+            headers: {
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify(this.state)
+        }).then(response => response.json())
+          .then(data => console.log(data))
+          .then(this.props.toggleForm)
+          .then(this.props.refresh)
+    }
+    handleChange = (event) => {
+        this.setState({ [event.target.name] : event.target.value });
+    }
+    handlePlatformsChange = (event, index) => {
+        const updatedPlatforms = this.state.platforms.concat([]);
+        updatedPlatforms[index] = event.target.value;
+        this.setState({platforms: updatedPlatforms});
+    }
+    addPlatform = (event) => {
+        const newPlatforms = this.state.platforms.concat([ "" ]);
+        this.setState({platforms:newPlatforms});
+    }
+    removePlatform = (index) => {
+        const newPlatforms = this.state.platforms.concat([]);
+        newPlatforms.splice(index, 1);
+        this.setState({ platforms: newPlatforms });
+    }
+    render(){
+        const platformInputs = this.state.platforms.map((platform, index) => {
+            return (
+                <div>
+                    <input name={index}
+                        type="text"
+                        // placeholder="Platform"
+                        placeholder={`Platform ${index + 1}`}
+                        value={platform}
+                        onChange={(event) => this.handlePlatformsChange(event, index)} 
+                        required/>
+                    <input className="remove-platform"
+                        type="button"
+                        value="-"
+                        onClick={()=> this.removePlatform(index)}/>
+                </div>
+                
+            )
+        })
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input name="title"
+                    type="text"
+                    placeholder="Title"
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                    required/>
+                <input name="developer"
+                    type="text"
+                    placeholder="Developer"
+                    value={this.state.developer}
+                    onChange={this.handleChange}/>
+                {platformInputs}
+                <input id="add_platform" 
+                    type="button" 
+                    value="Add Platform"
+                    onClick={this.addPlatform}/>
+                <input type="submit" value="Update Game"/>
+            </form>
+        )
+    }
+}
