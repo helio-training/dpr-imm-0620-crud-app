d465f38a87a652152cf7a766b3e3718df7b8eb8a
Wes Cutting <wesleycutting@gmail.com>
2020-08-05 14:17:10 -0600

    First Heroku Deployment

---
+++ public/precache-manifest.fce3b9d5745cc705cdede4eb49b46463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8ea60d01a5f8ca134f3a6b68e8792c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929372de97d6cffe49f1",
+    "url": "/static/css/main.a472acb4.chunk.css"
+  },
+  {
+    "revision": "eec87eb09a11b1256387",
+    "url": "/static/js/2.14a7cabd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14a7cabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929372de97d6cffe49f1",
+    "url": "/static/js/main.c47b188f.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
